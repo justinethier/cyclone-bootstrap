edfa5e5b097207ad46ab611a29a737e5e2539264
Justin Ethier <justin.ethier@gmail.com>
2015-12-09 20:44:36 -0500

    Automatically run via gdb until a crash occurs

--- debug2.sh
+++ debug2.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
-#for ((n=0;n<100;n++))
-#do 
-#  echo "Running cyclone $n"
-gdb -ex run --args ./cyclone icyc.scm 2>debug.out
+for ((n=0;n<100;n++))
+do 
+  echo "Running cyclone $n"
+  gdb -ex run --eval-command=quit --args ./cyclone icyc.scm 2>debug.out
 #gdb -ex run --args ./cyclone unit-tests.scm 2>debug.out
-#  if [ $? -ne 0 ]
-#  then
-#    break
-#  fi
-#done
+  if [ $? -ne 0 ]
+  then
+    break
+  fi
+done
