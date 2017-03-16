62edf67aecb6aad6daf2ca5a988a4bca9bf0321a
Justin Ethier <justin.ethier@gmail.com>
2017-03-16 14:12:17 +0000

    Sync

--- scheme/base.c
+++ scheme/base.c
@@ -25625,16 +25625,10 @@ static void __lambda_735(void *data, int argc, object self_736744, object k_7342
   return_closcall1(data,  k_734258,  x_731670);; 
 }
 
-static void __lambda_734(void *data, int argc, closure _, object k, object obj) { make_pair(p, obj, ((gc_thread_data *)data)->param_objs);
-        gc_thread_data *thd = (gc_thread_data *)data;
-        //Cyc_st_add(data, "scheme/base.sld:set-param-objs!");
-        //fprintf(stderr, "scheme/base.sld:set-param-objs!\n");
-        global_set((thd->param_objs), &p);
-        //thd->param_objs = (object)(&p);
-        // obj is on the stack, need to add it to write barrier
-        // to ensure it is transported to the heap
-        //add_mutation(data, &p, -1, obj);
-        return_closcall1(data, k, boolean_t);  }
+static void __lambda_734(void *data, int argc, closure _, object k, object obj) { gc_thread_data *thd = (gc_thread_data *)data;
+        make_pair(c, obj, thd->param_objs);
+        thd->param_objs = &c;
+        return_closcall1(data, k, &c);  }
 static void __lambda_733(void *data, int argc, closure _, object k) { gc_thread_data *thd = (gc_thread_data *)data;
         //Cyc_st_add(data, "scheme/base.sld:get-param-objs");
         return_closcall1(data, k, thd->param_objs);  }
