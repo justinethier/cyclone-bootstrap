2481613038a325946dd4f58a07fea65632f8f694
Justin Ethier <justin.ethier@gmail.com>
2017-08-11 10:13:50 +0000

    Sync

--- include/cyclone/types.h
+++ include/cyclone/types.h
@@ -779,9 +779,13 @@ typedef struct {
   FILE *fp;
   int mode;
   unsigned char flags;
+  unsigned int line_num;
+  unsigned int col_num;
   char *mem_buf;
   size_t mem_buf_len;
 } port_type;
+
+#define IO_BUF_LEN 1024
 
 /** Create a new port object in the nursery */
 #define make_port(p,f,m) \
@@ -792,6 +796,8 @@ typedef struct {
   p.fp = f; \
   p.mode = m; \
   p.flags = 0; \
+  p.line_num = 0; \
+  p.col_num = 0; \
   p.mem_buf = NULL; \
   p.mem_buf_len = 0;
 
@@ -803,7 +809,9 @@ typedef struct {
   p.fp = f; \
   p.mode = m; \
   p.flags = 1; \
-  p.mem_buf = NULL; \
+  p.line_num = 0; \
+  p.col_num = 0; \
+  p.mem_buf = malloc(IO_BUF_LEN); \
   p.mem_buf_len = 0;
 
 /**
