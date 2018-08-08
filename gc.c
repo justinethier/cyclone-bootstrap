1d205b9bc1fa5806b428456aa8a47e62e6c82b75
Justin Ethier <justin.ethier@gmail.com>
2018-08-08 12:19:34 -0400

    Sync

--- gc.c
+++ gc.c
@@ -541,7 +541,7 @@ size_t gc_convert_heap_page_to_free_list(gc_heap *h, gc_thread_data *thd)
 gc_heap *gc_sweep_fixed_size(gc_heap * h, int heap_type, gc_thread_data *thd)
 {
   short heap_is_empty;
-  object p, end;
+  object p;
   gc_free_list *q, *r, *s;
 #if GC_DEBUG_SHOW_SWEEP_DIAG
   gc_heap *orig_heap_ptr = h;
@@ -567,15 +567,14 @@ gc_heap *gc_sweep_fixed_size(gc_heap * h, int heap_type, gc_thread_data *thd)
     size_t remaining = h->size - (h->size % h->block_size); // - h->block_size; // Remove first one??
     char *data_end = h->data + remaining;
     heap_is_empty = 1; // Base case is an empty heap
-    end = (object)data_end;
-    p = h->data;
     q = h->free_list;
-    while (p < end) {
+    while (remaining) {
+      p = data_end - remaining;
       // find preceding/succeeding free list pointers for p
       for (r = (q?q->next:NULL); r && ((char *)r < (char *)p); q = r, r = r->next) ;
       if ((char *)q == (char *)p || (char *)r == (char *)p) {     // this is a free block, skip it
         //printf("Sweep skip free block %p remaining=%lu\n", p, remaining);
-        p = (object) (((char *)p) + h->block_size);
+        remaining -= h->block_size;
         continue;
       }
 #if GC_SAFETY_CHECKS
@@ -649,7 +648,7 @@ gc_heap *gc_sweep_fixed_size(gc_heap * h, int heap_type, gc_thread_data *thd)
       }
       //next->next = (gc_free_list *)(((char *) next) + h->block_size);
       //next = next->next;
-      p = (object) (((char *)p) + h->block_size);
+      remaining -= h->block_size;
     }
   }
   // Free the heap page if possible.
@@ -1915,6 +1914,7 @@ void gc_mut_cooperate(gc_thread_data * thd, int buf_len)
 #if GC_DEBUG_VERBOSE
       debug_print = 1;
 #endif
+//printf("DEBUG - mutator is cooperating\n");            
       // Mark thread "roots":
       // Begin by marking current continuation, which may have already
       // been on the heap prior to latest minor GC
@@ -2816,17 +2816,33 @@ void gc_mutator_thread_blocked(gc_thread_data * thd, object cont)
 
 void Cyc_apply_from_buf(void *data, int argc, object prim, object * buf);
 
+void gc_recopy_obj(object obj, gc_thread_data *thd) 
+{
+  // Temporarily change obj type so we can copy it
+  object fwd = forward(obj);
+  tag_type tag = type_of(fwd);
+  type_of(obj) = tag;
+
+fprintf(stderr, "\n!!! Recopying object %p with tag %d !!!\n\n", obj, tag);
+  // Copy it again
+  gc_copy_obj(fwd, obj, thd);
+  // Restore forwarding pointer tag
+  type_of(obj) = forward_tag;
+}
+
 /**
  * @brief Called explicitly from a mutator thread to let the collector know
  * that it has finished blocking. 
  * @param thd Mutator's thread data
  * @param result  Data returned by the blocking function
+ * @param maybe_copied An object used by the mutator while blocked that may 
+ *                     have been copied to the heap by the collector.
  *
  * In addition, if the collector cooperated on behalf of the mutator while 
  * it was blocking, the mutator will move any remaining stack objects to 
  * the heap and longjmp.
  */
-void gc_mutator_thread_runnable(gc_thread_data * thd, object result)
+void gc_mutator_thread_runnable(gc_thread_data * thd, object result, object maybe_copied)
 {
   char stack_limit;
   // Transition from blocked back to runnable using CAS.
@@ -2846,6 +2862,13 @@ void gc_mutator_thread_runnable(gc_thread_data * thd, object result)
     // Setup value to send to continuation
     thd->gc_args[0] = result;
     thd->gc_num_args = 1;
+    // Check if obj was copied while we slept
+    if (maybe_copied && 
+        is_object_type(maybe_copied) && 
+        gc_is_stack_obj(thd, maybe_copied) &&
+        type_of(maybe_copied) == forward_tag) {
+      gc_recopy_obj(maybe_copied, thd);
+    }
     // Move any remaining stack objects (should only be the result?) to heap
     gc_minor(thd, &stack_limit, thd->stack_start, thd->gc_cont, thd->gc_args,
              thd->gc_num_args);
