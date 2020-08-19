9af80b3a7299ef8af555e21db77bd60d091c3972
Justin Ethier <justin.ethier@gmail.com>
2020-08-19 10:05:46 -0400

    Sync

--- scheme/cyclone/common.c
+++ scheme/cyclone/common.c
@@ -318,7 +318,7 @@ Cyc_set_globals_changed((gc_thread_data *)data);
   __glo__85version_85_scheme_cyclone_common = boolean_f; 
   make_utf8_string_with_len(c_7335, "", 0, 0); 
   __glo__85version_91name_85_scheme_cyclone_common = &c_7335; 
-  make_utf8_string_with_len(c_7334, "0.20", 4, 4); 
+  make_utf8_string_with_len(c_7334, "0.21", 4, 4); 
   __glo__85version_91number_85_scheme_cyclone_common = &c_7334; 
 
   mclosure0(clo_7392, c_schemecyclonecommon_inlinable_lambdas); make_pair(pair_7391, find_or_add_symbol("c_schemecyclonecommon_inlinable_lambdas"), &clo_7392);
