709f3d0dba9ea60af223b5d8ef64030af84aae5d
Justin Ethier <justin.ethier@gmail.com>
2021-05-08 18:47:30 -0700

    Added header comment

--- build-wasm.sh
+++ build-wasm.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
+# Re-compile everything using EmScripten. This allows our libraries to be linked to Web Assembly programs.
 source ~/Documents/emsdk/emsdk_env.sh && emmake make -f Makefile-wasm polyfill && emmake make -f Makefile-wasm
